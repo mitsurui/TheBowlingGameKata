ed6af3c02982ac8bc21ef8a76d8000c151bc006d
johnlim <johnlim.dingwei@gmail.com>
2014-02-18 14:15:32 +0800

    World gives cell life.

---
+++ src/main/groovy/World.groovy
@@ -0,0 +1,15 @@
+class World {
+  void giveCellLife(Cell cell){
+
+  }
+
+  boolean isCellAlive(Cell cell){
+    return true;
+  }
+
+
+}
+
+class Cell {
+
+}
--- src/test/groovy/TestGameOfLife.groovy
+++ src/test/groovy/TestGameOfLife.groovy
@@ -1,7 +1,23 @@
 import spock.lang.*
 
+//Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+//Any live cell with two or three live neighbours lives on to the next generation.
+//Any live cell with more than three live neighbours dies, as if by overcrowding.
+//Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
 class TestGameOfLife extends Specification {
-  def "fail"() {
+  def "testWorldGivesCellLifeIsAlive"() {
+    Cell cell = new Cell();
+    World world = new World();
+    world.giveCellLife(cell);
+    expect:
+      world.isCellAlive(cell) == true;
+  }
+  @Ignore
+  def "testCellWithOneLiveNeighbourDiesOnNextTick"() {
+    World world = new World();
+    Cell cell = new Cell();
+    world.giveCellLife(cell);
     expect:
       false;
   } 
